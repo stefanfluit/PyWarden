05e449bac0983ff4da441c45c42e4ad8477900e7
Stefan Fluit <stefan@fluit-online.nl>
2022-09-18 21:59:53 +0200

    Add group ID fetcher

---
+++ src/pywarden/api/get_ids.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import requests
+import token
+from pywarden import handle_config
+
+def get_group_id(group_name):
+    BW_BEARER_TOKEN = token.get_bearer_token()
+    headers = {
+    'Authorization': 'Bearer {}'.format(BW_BEARER_TOKEN),
+    }
+    response = requests.get(handle_config.BITWARDEN_PUBLIC_API_URL + '/public/groups', headers=headers)
+    for group in response.json()['data']:
+        if group['name'] == group_name:
+            return(group['id'])
