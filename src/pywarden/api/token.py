264d54085a5380603f0c2b5a2f1afdcd8b48a81a
Stefan Fluit <stefan@fluit-online.nl>
2022-09-18 22:01:09 +0200

    Fetch bearer token

---
+++ src/pywarden/api/token.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import requests
+from pywarden import handle_config
+
+def get_bearer_token():
+    headers = {
+    'Content-Type': 'application/x-www-form-urlencoded',
+    }
+    data = 'grant_type=client_credentials&scope=api.organization&client_id={}&client_secret={}'.format(handle_config.BITWARDEN_ORGANIZATION_ID, handle_config.BITWARDEN_ORGANIZATION_SECRET)
+    response = requests.post(handle_config.BITWARDEN_AUTH_ENDPOINT, headers=headers, data=data)
+    return response.json()['access_token']
