6354eeb5fb8da6383e8d514bfabe2298eb64d0c4
Stefan Fluit <stefan.fluit@robinradar.com>
2022-09-19 11:32:08 +0200

    Fetch the version from the toml file

--- src/pywarden/pywarden/version.py
+++ src/pywarden/pywarden/version.py
@@ -1,6 +1,16 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+from pywarden import handle_config
+import os
+
 def pywarden_version():
-    pywarden_version = '0.0.2'
-    return(pywarden_version)
+    dir = handle_config.get_dir()
+    # Check if file pyproject.toml exists
+    if os.path.isfile(dir + '/' + 'pyproject.toml'):
+        # Read the file
+        with open(dir + '/' + 'pyproject.toml', 'r') as file:
+            pyproject_toml = file.read()
+        # Get the version number
+        pywarden_version = pyproject_toml.split('version = "')[1].split('"')[0]
+        return pywarden_version
