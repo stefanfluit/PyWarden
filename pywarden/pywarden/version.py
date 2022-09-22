e711329bca24b70bad19d1e502f082981910a1ec
Stefan Fluit <stefan.fluit@robinradar.com>
2022-09-22 16:00:51 +0200

    Maintain version number inside py files

--- pywarden/pywarden/version.py
+++ pywarden/pywarden/version.py
@@ -1,16 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from pywarden.pywarden import handle_config
-import os
-
 def pywarden_version():
-    dir = handle_config.get_dir()
-    # Check if file pyproject.toml exists
-    if os.path.isfile(dir + '/' + 'pyproject.toml'):
-        # Read the file
-        with open(dir + '/' + 'pyproject.toml', 'r') as file:
-            pyproject_toml = file.read()
-        # Get the version number
-        pywarden_version = pyproject_toml.split('version = "')[1].split('"')[0]
-        return pywarden_version
+    pywarden_version = "0.0.5"
+    return pywarden_version
