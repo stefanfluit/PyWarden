2eb972a254b35e942193cfe1fa9b7797b3f403ff
Stefan Fluit <stefan@fluit-online.nl>
2022-09-29 16:29:09 +0200

    Fetch version from config file

--- pywarden/pywarden/version.py
+++ pywarden/pywarden/version.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import requests
+import configparser
+
 def pywarden_version():
-    pywarden_version = "0.1.0"
-    return pywarden_version
+    fetch_url = "https://raw.githubusercontent.com/stefanfluit/PyWarden/main/setup.cfg"
+    with open ("setup.cfg", "w") as f:
+        f.write(requests.get(fetch_url).text)
+    config = configparser.ConfigParser()
+    config.read("setup.cfg")
+    return config["metadata"]["version"]
