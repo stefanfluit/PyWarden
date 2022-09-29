534231e0f02d6bc54de6cbc824beb6166268868c
Stefan Fluit <stefan@fluit-online.nl>
2022-09-29 12:22:42 +0200

    add a default message to prevent invalid model state

--- pywarden/cli/create_item.py
+++ pywarden/cli/create_item.py
@@ -12,7 +12,7 @@ from pywarden.logger import logger
 from pywarden.login import unlock
 from pywarden.cli import create_collection
 
-def bw_create_org_item(ITEM_NAME, ITEM_USERNAME, ITEM_PASSWORD, ITEM_URL, ITEM_NOTES, ITEM_FOLDER, ITEM_COLLECTION, ITEM_ORG_COLLECTION, ITEM_ACCESS_GROUP):
+def bw_create_org_item(ITEM_NAME, ITEM_USERNAME, ITEM_PASSWORD, ITEM_URL, ITEM_NOTES, ITEM_FOLDER, ITEM_COLLECTION, ITEM_ORG_COLLECTION, ITEM_ACCESS_GROUP, debug_mode=False, ITEM_URL_MATCH=None, ITEM_TOTP=None):
     # Ensure all variables are defined, error out if not.
     if ITEM_NAME is None:
         if ITEM_NAME != "":
@@ -50,26 +50,28 @@ def bw_create_org_item(ITEM_NAME, ITEM_USERNAME, ITEM_PASSWORD, ITEM_URL, ITEM_N
     get_template_json['collectionIds'] = collectionIds
 
     # Set login variables
-    if ITEM_URL != None:
-        if ITEM_URL != '':
-            get_login_template_json['match'] = True
-            get_login_template_json['uri'] = ITEM_URL
-        else:
-            get_login_template_json['match'] = None
-            get_login_template_json['uri'] = None
+    # Error if ITEM_URL_MATCH is true, but ITEM_URL is not defined
+    if ITEM_URL_MATCH == True:
+        if ITEM_URL is None:
+            if ITEM_URL != "":
+                logger.pywarden_logger(Payload="ITEM_URL is not defined, but --match was provided", Color="red", ErrorExit=True, Exit=False)
+    get_login_template_json['match'] = ITEM_URL_MATCH
+    get_login_template_json['uri'] = ITEM_URL
 
     # Set login item variables
     get_login_item_template_json['username'] = ITEM_USERNAME
     get_login_item_template_json['password'] = ITEM_PASSWORD
     get_login_item_template_json['uris'] = [get_login_template_json]
-    get_login_item_template_json['totp'] = None
+    get_login_item_template_json['totp'] = ITEM_TOTP
 
     # Set notes
     if ITEM_NOTES != None:
         if ITEM_NOTES != '':
             get_template_json['notes'] = ITEM_NOTES
         else:
-            get_template_json['notes'] = None
+            get_template_json['notes'] = "Entry created using PyWarden."
+    else:
+        get_template_json['notes'] = "Entry created using PyWarden."
 
     # Combine
     get_template_json['login'] = get_login_item_template_json
@@ -77,6 +79,11 @@ def bw_create_org_item(ITEM_NAME, ITEM_USERNAME, ITEM_PASSWORD, ITEM_URL, ITEM_N
     # Encode the json into base64
     b64 = str(base64.b64encode(json.dumps(get_template_json).encode('utf-8')))
     b64 = b64.replace("b'", "").replace("'", "")
+
+    # If debug mode is true, print the json
+    if debug_mode == True:
+        print(json.dumps(get_template_json, indent=4))
+
     subprocess.check_output(f'bw create item {b64} --pretty --nointeraction --organizationid={get_items.get_organization_id()} --session={unlock.bw_unlock()}', shell=True, encoding='utf-8')
     sync.bw_sync()
     if get_items.check_if_object_exists('item', ITEM_NAME) == True:
